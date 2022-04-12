347f2c0bc5a9c03b4311eb4c289ca1413de0b238
timruhkopf <timruhkopf@gmail.com>
2022-04-12 19:08:20 +0200

    Added 1. Pipeline 2. TableTransforms (ToTensor, Drop, Convert, Transform, Scalar, Replace) 3. Added AlgorithmMetaFeatures & DatasetMetaFeatures classes.    Notice, that algorithm_meta_features.py main already has the config to make this a proper dataset. TODO: move this to hydra config in src/main.py

---
+++ mf_gravitas/src/data/subdatasets/dataset_meta_features.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import torch
+from torch.utils.data import Dataset
+
+from mf_gravitas.src.data.preprocessings.transformpipeline import TransformPipeline
+
+
+class DatasetMetaFeatures(Dataset):
+    def __init__(self, path, transforms: TransformPipeline = None, *args, **kwargs):
+        self.path = path
+        self.transform = transforms
+
+        self.df = pd.read_csv(path, *args, **kwargs)
+        self.names = list(self.df.index)
+
+        if transforms is not None:
+            if transforms.fitted:
+                self.transformed_df = self.transform.transform(self.df)
+            else:
+                self.transformed_df = self.transform.fit(self.df)
+
+    def __len__(self):
+        return len(self.transformed_df)
+
+    def __getitem__(self, idx):
+        if not isinstance(self.transformed_df, torch.Tensor):
+            raise ValueError('You are trying to index a dataframe not tensor!')
+        return self.transformed_df[idx]
