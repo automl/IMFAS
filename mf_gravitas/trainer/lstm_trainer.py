bf64c52047bbadec7a7e28f084a1d584705aba26
timruhkopf <timruhkopf@gmail.com>
2022-06-02 16:09:36 +0200

    [BUGFIX] glmnet now has its own algo meta config

--- mf_gravitas/trainer/lstm_trainer.py
+++ mf_gravitas/trainer/lstm_trainer.py
@@ -1,6 +1,5 @@
-from nbformat import read
 import torch
-import pdb
+
 
 class Trainer_Ensemble_lstm:
     def __init__(self, model, loss_fn, ranking_fn, optimizer, test_lim=5):
@@ -16,7 +15,7 @@ class Trainer_Ensemble_lstm:
         self.test_lim = test_lim
         self.readout = torch.nn.Linear(model.shared_hidden_dims[-1], model.algo_dim)
 
-        #self.n_slices = self.model.n_fidelities
+        # self.n_slices = self.model.n_fidelities
 
         self.loss_kwargs = {
             'ranking_fn': self.ranking_fn,
@@ -27,41 +26,38 @@ class Trainer_Ensemble_lstm:
         test_shared_losses = []
 
         for _, data in enumerate(test_dataloader):
-
             # Seperate labels into indices of slices
             D0 = data[0].to(self.model.device)
 
-           
             # Only feed limited fidelities for test
-            labels = data[1][:,:self.test_lim,:].to(self.model.device)
-         
+            labels = data[1][:, :self.test_lim, :].to(self.model.device)
+
             # Get Embeddings
             self.model.eval()
             with torch.no_grad():
-
                 # Feed the lstm till penultimate fidelitie
                 # calculate embedding
                 shared_D0, lstm_D0 = self.model.forward(
-                                    dataset_meta_features= D0,
-                                    fidelities = labels
-                                )
+                    dataset_meta_features=D0,
+                    fidelities=labels
+                )
 
                 # Get the loss for lstm output
                 lstm_loss = self.loss_fn(
-                        pred=lstm_D0,
-                        target=data[1][:,-1,:],
-                        **self.loss_kwargs
-                    )
+                    pred=lstm_D0,
+                    target=data[1][:, -1, :],
+                    **self.loss_kwargs
+                )
 
                 self.readout.load_state_dict(self.model.lstm_network.readout.state_dict())
                 D0_rank = self.readout.forward(shared_D0.detach())
 
-                #Get the loss for lstm output
+                # Get the loss for lstm output
                 shared_loss = self.loss_fn(
-                        pred=D0_rank.detach(),
-                        target=data[1][:,-1,:],
-                        **self.loss_kwargs
-                    )
+                    pred=D0_rank.detach(),
+                    target=data[1][:, -1, :],
+                    **self.loss_kwargs
+                )
 
                 test_lstm_losses.append(lstm_loss)
                 test_shared_losses.append(shared_loss)
@@ -69,35 +65,30 @@ class Trainer_Ensemble_lstm:
         self.losses[f'lstm_loss'] = torch.stack(test_lstm_losses).mean()
         self.losses[f'shared_loss'] = torch.stack(test_shared_losses).mean()
 
-
-
     def step_next(self):
         self.step += 1
 
     def train(self, train_dataloader):
 
         for _, data in enumerate(train_dataloader):
-
             self.optimizer.zero_grad()
             # Dataset meta features and final  slice labels
             D0 = data[0].to(self.model.device)
 
-            labels = data[1][:,:-1,:]
-            
+            labels = data[1][:, :-1, :]
+
             # calculate embedding
             shared_D0, lstm_D0 = self.model.forward(
-                                    dataset_meta_features= D0,
-                                    fidelities = labels
-                                )
+                dataset_meta_features=D0,
+                fidelities=labels
+            )
 
             lstm_loss = self.loss_fn(
-                            pred=lstm_D0,
-                            target=data[1][:,-1,:],
-                            **self.loss_kwargs
-                        )
-
-            
+                pred=lstm_D0,
+                target=data[1][:, -1, :],
+                **self.loss_kwargs
+            )
 
             lstm_loss.backward()
-            
+
             self.optimizer.step()
