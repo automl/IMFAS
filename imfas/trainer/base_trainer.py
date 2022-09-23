64cfb8b611e411049e794462cebb13750594b6ba
timruhkopf <timruhkopf@gmail.com>
2022-09-23 08:55:42 +0200

    [Feature][Refactor] SliceEvaluator is more tightly coupled with Basetrainer, Basetrainer cleaned & simplified.
    Basetrainer accepts nn.Modules and other kinds of models;
    nn.Modules are trained, other models such as sh are directly forwarded to the evaluation protocol
    SliceEvaluator properly tracks the max-fidelity evaluation protocol

--- imfas/trainer/base_trainer.py
+++ imfas/trainer/base_trainer.py
@@ -1,5 +1,6 @@
-from typing import Callable, Dict, Optional
+from typing import Callable, Dict, Optional, Union
 
+import torch.nn as nn
 import torch.optim
 import wandb
 from hydra.utils import instantiate
@@ -9,24 +10,23 @@ from tqdm import tqdm
 
 class BaseTrainer:
     def __init__(
-        self,
-        model: torch.nn.Module,
-        optimizer: torch.optim.Optimizer = None,
-        # callbacks_end: List[Callable] = None
+            self,
+            model,
+            optimizer: Optional[torch.optim.Optimizer] = None,
     ):
         """
 
         :param model: torch.nn.Module
         :param optimizer: A partially instantiated object accepting model.parameters()
-        :param callbacks_end: list of functions to be called at the end of each epoch
+
         """
-        self._step = 0  # FIXME: @Aditya: why exactly doesn't it suffice to have the epoch?
+        self._step = 0
         self.model = model
         self.device = self.model.device
 
-        if optimizer is not None:
+        if isinstance(self.model, nn.Module):
+            # partial instantiation from hydra!
             self.optimizer = optimizer(self.model.parameters())
-        # self.callbacks_end = callbacks_end
 
     @property
     def step(self):
@@ -41,12 +41,9 @@ class BaseTrainer:
         """define one epoch of training"""
         # TODO: incorporate callbacks to the training loop (before and after each epoch, and )
         #  at every (k-th) step?
-        for _, data in enumerate(train_loader):
-            # Data parsing
-            X, y = data  # assuming a dict of tensors here for each
+        for _, (X, y) in enumerate(train_loader):
             self.to_device(X)
-            self.to_device(y)  # fixme: move to device in fwd call (to allow for data prep such as
-            # masking?)
+            self.to_device(y)
 
             self.optimizer.zero_grad()
 
@@ -59,67 +56,59 @@ class BaseTrainer:
             # FIXME: y needs to be explicit or have a strong conventioN
             self.optimizer.step()
 
-            # Log the training loss
+        # Log the training loss at the end of the epoch
         if self.step % log_freq == 0:
-            wandb.log({"trainingloss": loss}, step=self.step)  # fixme: every training epoch!
+            wandb.log({"trainingloss": loss}, step=self.step)
 
         self._step += 1
 
-    def evaluate(self, test_loader, valid_loss_fn, aggregate_fn=None):
+    def evaluate(self, test_loader, valid_loss_fn, function_name):
         """evaluate the model on the test set after epoch ends for a single validation function"""
-        losses = []
-        self.model.eval()  # Consider: since we can ask the state of the model
-        # we can actuall use this to change the forward to track particular values
 
         with torch.no_grad():
-            for _, data in enumerate(test_loader):
+            losses = torch.zeros(len(test_loader))
+            for i, data in enumerate(test_loader):
                 X, y = data
                 self.to_device(X)
                 self.to_device(y)
 
                 y_hat = self.model.forward(**X)
-                loss = valid_loss_fn(y_hat, y["final_fidelity"])
+                losses[i] = valid_loss_fn(y_hat, y["final_fidelity"])
 
-                losses.append(loss)
-
-        return losses[0] if aggregate_fn is None else aggregate_fn(losses)
+            wandb.log({function_name: losses.mean()}, step=self.step)
 
     def run(
-        self,
-        train_loader,
-        test_loader,
-        epochs,
-        train_loss_fn,
-        log_freq=5,
-        valid_loss_fns: Dict[str, Callable] = None,
-        aggregate_fn: Optional[Callable] = None,
+            self,
+            train_loader: torch.utils.data.DataLoader,
+            test_loader: torch.utils.data.DataLoader,
+            epochs: int,
+            train_loss_fn: Union[Callable, DictConfig],
+            log_freq: int = 5,
+            valid_loss_fns: Dict[str, Callable] = None,
     ):
-        """Main loop including training & test evaluation, all of which report to wandb"""
+        """
+        Main loop including training & test evaluation, all of which report to wandb
+
+        :returns None, but tracks using wandb both the trainloss all the validation losses
+        """
 
         # Class & functional interface
-        train_loss_fn = instantiate(train_loss_fn)
+        if isinstance(train_loss_fn, DictConfig):
+            train_loss_fn = instantiate(train_loss_fn)
 
         for epoch in tqdm(range(epochs), desc='Training epochs'):
-            self.train(train_loader, epoch, train_loss_fn)
+            if isinstance(self.model, nn.Module):
+                self.train(train_loader, epoch, train_loss_fn)
 
             # move this  parameter hist tracker to a callback?
             # for k, t in self.model.state_dict().items():
             #     wandb.log({k: wandb.Histogram(torch.flatten(t))}, step=self.step)
 
             if valid_loss_fns is not None and self.step % log_freq == 0:
-                # End of epoch validation loss tracked in wandb
-
-                for k, fn in valid_loss_fns.items():
-
-                    if isinstance(fn, DictConfig):
+                for fn_name, fn in valid_loss_fns.items():
+                    if isinstance(fn, DictConfig):  # fixme: can we remove this?
                         fn = instantiate(fn)
 
-                    loss = self.evaluate(test_loader, fn, aggregate_fn)
-                    # self.losses[k].append(loss)
-
-                    wandb.log({k: loss.item()}, step=self.step)  # FIXME @Aditya, this fails in
-                    # plackett-luce test
-
-            # # execute other callbacks on the end of each epoch
-            # for callback in self.callbacks_end:
-            #     callback(trainer=self)
+                    self.model.eval()
+                    self.evaluate(test_loader, fn, fn_name)
+                    self.model.train()
--- imfas/trainer/slice_evaluator.py
+++ imfas/trainer/slice_evaluator.py
@@ -1,13 +1,19 @@
+import warnings
 from functools import partial
-from typing import List
+from typing import List, Callable
+
+import torch
 
 from imfas.trainer.base_trainer import BaseTrainer
 
 
 class SliceEvaluator(BaseTrainer):
-    def __init__(self, model, max_fidelities: List, masking_fn=None, *args, **kwargs):
+    def __init__(self, model, max_fidelities: List, masking_fn: Callable, *args, **kwargs):
         """
-        This class is designed to evaluate the model during test time on a fixed
+        This BaseTrainer subclass trains as usual, but evaluates the model on a set of slices of
+        different length. In particular, it evaluates the model's capability to "forsee" the future
+        conditioned on only partial learning curves
+        This class is designed to ev aluate the modelduring test time on a fixed
         number of fidelities (i.e. there is a highest accessible fidelity (via masking).
 
         In particular it is build with successive halving in mind. Consider its definition.
@@ -16,33 +22,89 @@ class SliceEvaluator(BaseTrainer):
         self.max_fidelities = max_fidelities
         self.masking_fn = masking_fn
 
-    def evaluate(self, test_loader, valid_loss_fn, *args, **kwargs):
-
-        n_algos = len(test_loader.dataset.meta_algo)
-        if hasattr(self.model, "budgets"):
-            # SUCCESSIVE HALVING speciality
-            budgets = self.model.budgets
-
+    def evaluate(self, test_loader, valid_loss_fn, fn_name, *args, **kwargs):
         for fidelity in self.max_fidelities:
-
-            if hasattr(self.model, "budgets"):
-                # SUCCESSIVE HALVING speciality
-                self.model.__init__(budgets, eta=self.model.eta)
-
             test_loader.dataset.masking_fn = partial(self.masking_fn, max_fidelity=fidelity)
 
-            losses = list()
-
-            for X, y in test_loader:
+            losses = torch.zeros(len(test_loader))
+            for i, (X, y) in enumerate(test_loader):
                 self.to_device(X)
                 self.to_device(y)
 
                 y_hat = self.model.forward(**X)
-                losses.append(valid_loss_fn(y_hat, y["final_fidelity"]))
+                losses[i] = valid_loss_fn(y_hat, y["final_fidelity"])
 
-            return losses
+            wandb.log({f"max fidelity: {fn_name}": losses.mean(), 'fidelity': fidelity})
+
+    def run(self, train_loader, test_loader, epochs, log_freq, *args, **kwargs):
+        # TODO: check what happens when this condition is not met
+        if epochs != log_freq:
+            warnings.warn("SliceEvaluator is intended to be run with log_freq == epochs")
+        super().run(train_loader, test_loader, epochs=epochs, log_freq=log_freq, *args, **kwargs)
 
 
-class SliceEvaluatorSH(SliceEvaluator):
-    def train(self, *args, **kwargs):
-        return None
+if __name__ == '__main__':
+    """
+    Experiment to run SH baseline on the fixed fidelity slices (i.e. maximum available fidelity)
+    and compare that against a trainable model, that is evaluated on the same maximum slices.
+    """
+
+    from torch.utils.data import DataLoader
+
+    from imfas.models.baselines.successive_halving import SuccessiveHalving
+    from imfas.losses.spearman import SpearmanLoss
+    from imfas.utils.masking import mask_lcs_to_max_fidelity
+    from imfas.data.lcbench.example_data import train_dataset, test_dataset
+
+    import wandb
+
+    wandb.init(entity="tnt", mode='online', project='imfas-iclr', job_type='train')
+
+    # (evaluate SH) ---------------------------------------------
+    model = SuccessiveHalving(budgets=[5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 51], eta=2)
+    test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)  # batch=1, because of SH
+
+    sliceevaluator = SliceEvaluator(
+        model,
+        max_fidelities=[5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 51],
+        # creates a learning curve for these fidelities
+        masking_fn=mask_lcs_to_max_fidelity
+    )
+
+    # sliceevaluator.evaluate(test_loader, valid_loss_fn=SpearmanLoss(), fn_name='spearman')
+
+    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+
+    # (Train + evaluate another model) ----------------------------
+    from imfas.models.imfas_wp import IMFAS_WP
+    from imfas.utils.mlp import MLP
+
+    n_algos = 58
+    n_meta_features = 107
+    model = IMFAS_WP(
+        encoder=MLP(hidden_dims=[n_meta_features, 300, 200]),
+        decoder=MLP(hidden_dims=[200, n_algos]),
+        input_dim=n_algos,
+        n_layers=2
+    )
+    # model = PlackettTest(encoder=MLP(hidden_dims=[n_meta_features, 100, n_algos])) # constant in
+    # fidelity
+    sliceevaluator = SliceEvaluator(
+        model,
+        max_fidelities=[5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 51],
+        masking_fn=mask_lcs_to_max_fidelity,
+        optimizer=partial(torch.optim.Adam, lr=1e-3),
+    )
+
+    # NOTICE: for wandb tracking to be sensible, we need to train the model fully first.
+    epochs = 1000
+    sliceevaluator.run(
+        train_loader,
+        test_loader,
+        train_loss_fn=SpearmanLoss(),
+        valid_loss_fns={"spearman": SpearmanLoss()},
+        epochs=epochs,  # <----
+        log_freq=epochs  # <----
+    )
+
+    print('done')
