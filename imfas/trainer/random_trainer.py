b2dfa2fe31e4b3d4b205c81fff53010783d713ef
timruhkopf <timruhkopf@gmail.com>
2022-11-05 13:06:26 +0100

    [Feature] Random trainer; providing a new analysis on the regret distribution per dataset in the test dataset.
    Signed-off-by: timruhkopf <timruhkopf@gmail.com>

---
+++ imfas/trainer/random_trainer.py
@@ -0,0 +1,42 @@
+import wandb
+from hydra.utils import instantiate
+from tqdm import tqdm
+
+from imfas.trainer.base_trainer import BaseTrainer
+
+
+class RandomTrainer(BaseTrainer):
+    def __init__(self, model, reps, **kwargs):
+
+        self.reps = reps
+        self.model = model
+        self.device = self.model.device
+        self._step = 0
+
+    def train(self, train_loader, epochs, loss_fn, log_freq=1) -> None:
+        """define one epoch of training"""
+        pass
+
+    def validate(self, valid_loader, valid_loss_fn, function_name):
+        pass
+
+    def test(self, test_loader, test_loss_fns, **kwargs):
+
+        test_loss_fns = {f_name: instantiate(loss_fn) for f_name, loss_fn in test_loss_fns.items()}
+        print(f'test_loss_fns: {test_loss_fns}')
+        for (X, y) in tqdm(test_loader, desc=f'Test Dataset i'):
+            self.to_device(X)
+            self.to_device(y)
+            for reps in tqdm(range(self.reps), desc='random_reps'):
+                y_hat = self.model.forward(**X)
+
+                for f_name, loss_fn in test_loss_fns.items():
+                    # loss_fn = instantiate(loss_fn)
+
+                    loss = loss_fn(y_hat, y["final_fidelity"])
+
+                    # track with wandb
+                    if reps % 4 == 0:
+                        wandb.log({f"test/{f_name}": loss}, step=self.step)
+
+                self._step += 1
