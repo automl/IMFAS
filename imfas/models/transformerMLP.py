b92dc62ff0eae2d88faf0d69d43e0a170753e58c
timruhkopf <timruhkopf@gmail.com>
2022-11-10 15:22:41 +0100

    [BUG] TransformerMLP produces nan (even after the prepended mask) on yahpo
    Signed-off-by: timruhkopf <timruhkopf@gmail.com>

--- imfas/models/transformerMLP.py
+++ imfas/models/transformerMLP.py
@@ -3,6 +3,8 @@ from typing import Optional
 import torch
 from torch import nn
 
+from imfas.utils import MLP
+
 
 # marius proposal: prepend the dataset meta features to the sequence.
 
@@ -12,9 +14,13 @@ class IMFASTransformerMLP(nn.Module):
 
             positional_encoder,
             transformer_lc,
-            decoder,
+            # decoder,
             device,
+            decoder_hidden_dims: Optional[list],
+            n_algos: int,
+            n_fidelities: int,
             dataset_metaf_encoder: Optional[nn.Module] = None,
+
             **kwargs  # placeholder to make configuration more convenient
     ):
         """This model class defines the computational macro graph of the IMFAS model.
@@ -22,11 +28,15 @@ class IMFASTransformerMLP(nn.Module):
         super(IMFASTransformerMLP, self).__init__()
 
         self.dataset_metaf_encoder = dataset_metaf_encoder
+        dmetaf_encoder_outdim = self.dataset_metaf_encoder.layers[-2].weight.shape[-2]
         self.positional_encoder = positional_encoder
 
         self.transformer_lc = transformer_lc
 
-        self.decoder = decoder
+        # n_fidelities + 1 for nan safeguard
+        decoder_input_dim = dmetaf_encoder_outdim + (n_fidelities + 1) * n_algos
+        self.decoder = MLP(hidden_dims=[decoder_input_dim, *decoder_hidden_dims, n_algos])
+
         self.device = device
 
         self.check_conformity()
@@ -46,32 +56,42 @@ class IMFASTransformerMLP(nn.Module):
 
         # prepend a zero timestep to the learning curve tensor if n_fidelities is uneven
         # reasoning: transformer n_heads must be a devisor of d_model
-        if learning_curves.shape[-2] % 2 == 1:
-            # fixme: untested code
-            learning_curves = torch.cat(
-                (torch.zeros_like(learning_curves[:, :, :1]), learning_curves), dim=-2)
-
-            mask = torch.cat(
-                (torch.zeros_like(mask[:, :, :1]), mask), dim=-2)
+        # if learning_curves.shape[-2] % 2 == 1:
+        #     # fixme: untested code
+        #     learning_curves = torch.cat(
+        #         (torch.zeros_like(learning_curves[:, :, :1]), learning_curves), dim=-2)
+        #
+        #     mask = torch.cat((torch.zeros_like(mask[:, :, :1]), mask), dim=-2)
 
         pos_learning_curves = self.positional_encoder(learning_curves)
+
+        # safeguard against nan values resulting from unobserved learning curves,
+        # by prepending a zero token at the beginning of the fidelity sequence
+        mask = torch.cat([torch.ones(1, self.n_algos, 1), mask], dim=-1)
+        pos_learning_curves = torch.cat(
+            [torch.zeros(1, self.n_algos, 1), pos_learning_curves],
+            dim=-1
+        )
+
+        # batch trick: make the algorithms batch dim to allow for parallelization
+        # of independently attending to the learning curves of different algorithms
+        pos_learning_curves = pos_learning_curves.permute(1, 2, 0)
+        mask = mask.permute(1, 2, 0)
 
         lc_encoding = self.transformer_lc(
             # reshape the learning curve tensor to trick the transformer in believing
             # the algo dim is the batch dim
             # alter mask encoding (since pytorch expects mask that indicates missing values)
-            pos_learning_curves.permute(1, 2, 0),
+            pos_learning_curves,
 
             # same as above regarding permutation. But we need to invert & convert the mask,
             # since pytorch expects mask that indicates missing values & type bool,
             # such that mask.masked_fill(~mask.bool(), float('-inf')) is performed directly before
-            # softmax in the attention head
-            src_key_padding_mask=~mask.bool().permute(1, 2, 0).view(self.n_algos, self.n_fidelities)
-            # make it a
-            # 2d mask
+            # softmax in the attention head # +1 due to the prepended zero token!
+            src_key_padding_mask=~mask.bool().view(self.n_algos, self.n_fidelities + 1)
         )
 
-        return lc_encoding.permute(0, 1, 2)
+        return lc_encoding.permute(0, 1, 2)  # undo the batch trick
 
     def forward(self, learning_curves: torch.Tensor, mask: torch.Tensor,
                 dataset_meta_features: Optional[torch.Tensor] = None, **kwargs):
--- main.py
+++ main.py
@@ -12,7 +12,6 @@ import torch
 
 OmegaConf.register_new_resolver("device_ident", lambda _: torch.device(
     "cuda" if torch.cuda.is_available() else "cpu"))
-
 OmegaConf.register_new_resolver("add", lambda *numbers: sum(numbers))
 OmegaConf.register_new_resolver("len", lambda l: len(l))
 OmegaConf.register_new_resolver("range", lambda start, stop, step: list(range(start, stop, step)))
